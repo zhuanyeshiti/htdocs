dee12344a37e7dc141382f5b75bdad5d55ff5fc6
zhuanyeshiti <liuhongwei123456@gmail.com>
2019-04-03 00:45:33 +0800

    Create navig.php

---
+++ navig/navig.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<body style="padding-top:0px;margin-top:0px">
+<div style="cursor:default;width:auto;padding-top:0px;margin-top:0px;background:orange;text-align:center;font-size:20px;color:black;">
+<p style="padding-top:0px;margin-top:0px;-moz-user-select:none;-khtml-user-select:none;user-select:none">WELCOME TO MAIN PAGE</p>
+<br/>
+</div>
+</body>
+<div style="width:90%;height:90%;margin:auto;text-align:center;background:#0ff">
+</br>
+<h>搜索网站</h>
+<button>添加</button><button>删除</button>
+</br>
+</br>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button2</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button2</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button2</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button2</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button2</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button2</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button2</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button2</button>
+<p></p>
+</br>
+<h>搜索网站</h>
+<button>添加</button><button>删除</button>
+</br>
+</br>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button2</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button2</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button2</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button2</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button2</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button2</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button2</button>
+<button style="padding-left:0px;padding-right:0px;margin-left:0px;marging-right:0px;width:24%">button2</button>
+<p></p>
+</br>
+
+
+</div>
+
+
+
+
+</html>
